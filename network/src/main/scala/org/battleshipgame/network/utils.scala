e8c0c43f46136e9b80c85b8ef6c21058eef26492
Kirill Bogatikov <kivi15penza@gmail.com>
2020-02-22 02:35:20 +0300

    utils for network

---
+++ network/src/main/scala/org/battleshipgame/network/utils.scala
@@ -0,0 +1,47 @@
+package org.battleshipgame.network
+
+import java.util.Base64
+
+import scala.language.postfixOps
+
+object ShotParser {
+    private val DELIMITER = ","
+    
+    def parse(string :String) :Shot = {
+        var parts = string split(DELIMITER)
+        
+        if (parts.length != 2) return null
+        
+        return new Shot(parts(0) toInt, parts(1) toInt)
+    }
+    
+    def stringify(shot :Shot) :String = {
+        var builder = new StringBuilder()
+        builder append(shot.x) append(DELIMITER) append(shot.y)
+        
+        return builder toString()
+    }
+}
+
+object GameIdParser {
+    private val DELIMITER = "|"
+    
+    def parse(string :String) :GameId = {
+        val bytes = Base64.getDecoder().decode(string.getBytes())
+        val decoded = new String(bytes)
+        
+        var parts = decoded split(DELIMITER)
+        
+        return new GameId(parts(0), parts(1))
+    }
+    
+    def stringify(gameId :GameId) :String = {
+        val builder = new StringBuilder()
+        builder append(gameId hash) append(DELIMITER) append(gameId connection)
+        
+        var string = builder toString()
+        val bytes = Base64.getEncoder.encode(string getBytes())
+        
+        return new String(bytes)
+    }
+}
