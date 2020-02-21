2132a0aac872e31fbd725630e6b5c00984aab786
Kirill Bogatikov <kivi15penza@gmail.com>
2020-02-22 02:35:07 +0300

    network models

---
+++ network/src/main/scala/org/battleshipgame/network/models.scala
@@ -0,0 +1,24 @@
+package org.battleshipgame.network
+
+case class Packet(val hash: String, val group: String, val value: String) { }
+
+case class GameId(val hash: String, val connection: String) { }
+
+object Shot {
+    val SHOT = "shotpos"
+    val HURT = "shothurt"
+    val KILL = "shotkill"
+    val MISS = "shotmiss"
+}
+
+case class Shot(val x: Int, val y: Int) { }
+
+object Auth {
+    val HOW_ARE_YOU = "helloman, how are you?"
+    val I_M_FINE = "helloman, i'm fine"
+}
+
+object EndGame {
+    val WIN = "winner"
+    val LOSE = "loser"
+}
