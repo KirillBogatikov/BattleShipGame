c3a6aee5f591eb02faafffb3b875e27234b94e12
Kirill Bogatikov <kivi15penza@gmail.com>
2020-02-22 02:35:38 +0300

    EventListener trait

---
+++ network/src/main/scala/org/battleshipgame/network/EventListener.scala
@@ -0,0 +1,9 @@
+package org.battleshipgame.network
+
+trait EventListener {
+    def onFriendConnected() :Unit
+    def onConnectedToFriend() :Unit
+    def onShot(shot :Shot) :String
+    def onGameEnd(win :Boolean) :Unit
+    def onShotResult(result :String) :Unit
+}
