f8409c845ebb6db1e3cf985db8bcd91a6c6bda05
Kirill Bogatikov <kivi15penza@gmail.com>
2020-02-22 02:35:51 +0300

    HashGenerator util

---
+++ network/src/main/scala/org/battleshipgame/HashGenerator.scala
@@ -0,0 +1,19 @@
+package org.battleshipgame
+
+import scala.language.postfixOps
+import scala.util.Random
+
+object HashGenerator {
+    private val ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    private val random = new Random()
+    
+    def next() :String = {
+        val builder = new StringBuilder()
+        for(i <- 0 until 6) {
+            val j = random nextInt(ALPHABET length)
+            builder append(ALPHABET(j))
+        }
+        
+        return builder toString()
+    }
+}
