b14edaad5dd354013d1b7aa89576b56b075e2cc2
Kirill Bogatikov <kivi15penza@gmail.com>
2020-02-22 02:36:09 +0300

    Networker trait

---
+++ network/src/main/scala/org/battleshipgame/network/Networker.scala
@@ -0,0 +1,8 @@
+package org.battleshipgame.network
+
+trait Networker {
+   def receive(hash: String) :Packet
+   def send(packet: Packet) :Unit
+   def useGameHost(gameId: GameId) :Unit
+   def gameId(hash: String) :GameId
+}
