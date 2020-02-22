faca9c2ce207f5b36976d77121d38dca23ab6129
Kirill Bogatikov <kivi15penza@gmail.com>
2020-02-23 00:41:00 +0300

    onKeyEnter renamed to onKeyPress, now it accepts Int keyCode

--- render/src/main/scala/org/battleshipgame/render/InputListener.scala
+++ render/src/main/scala/org/battleshipgame/render/InputListener.scala
@@ -1,7 +1,7 @@
 package org.battleshipgame.render
 
 trait InputListener {
-    def onKeyEnter(key: Char): Unit
+    def onKeyPress(key: Int): Unit
     def onMouseMove(x: Int, y: Int): Unit
     def onClick(x: Int, y: Int): Unit
 }
