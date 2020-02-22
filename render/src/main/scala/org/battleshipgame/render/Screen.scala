b35733a01dcacfec22dba09755dd1472c2b88ecf
Kirill Bogatikov <kivi15penza@gmail.com>
2020-02-22 23:54:47 +0300

    Screen trait, StartScreen abstract class

---
+++ render/src/main/scala/org/battleshipgame/render/Screen.scala
@@ -0,0 +1,18 @@
+package org.battleshipgame.render
+
+import scala.language.postfixOps
+import org.battleshipgame.ui.ContentResolver
+
+trait Screen {
+    def content(): ContentResolver
+    def size(): Size
+    def render(renderer: Renderer): Unit
+    def update(renderer: Renderer): Unit
+        
+    protected def button(renderer: Renderer, button: View): Unit = {
+        renderer fill(content primaryColor)
+        renderer stroke(false)
+        renderer rectangle(button rectangle)
+        renderer text(button rectangle, button text, button textSize)
+    }
+}
---
+++ render/src/main/scala/org/battleshipgame/ui/StartScreen.scala
@@ -0,0 +1,52 @@
+package org.battleshipgame.ui
+
+import scala.language.postfixOps
+import org.battleshipgame.render.Rectangle
+import org.battleshipgame.render.Renderer
+import org.battleshipgame.render.Screen
+import org.battleshipgame.render.View
+import org.battleshipgame.render.InputListener
+import org.battleshipgame.render.Point
+
+object StartScreen {
+    protected var instance: StartScreen = null
+    
+    def impl(): StartScreen = {
+        return instance;
+    }
+}
+
+abstract class StartScreen extends Screen with InputListener {
+    StartScreen.instance = this
+    
+    def buttons(): List[View]
+    
+    override def render(renderer: Renderer): Unit = {
+        renderer begin()
+        
+        renderer image(new Rectangle(0, 0, size width, size height), content background)
+        
+        buttons foreach(view => button(renderer, view))
+        
+        renderer end()
+    }
+    
+    override def update(renderer: Renderer): Unit = {
+        //TODO: hover button
+    }
+    
+    override def onKeyEnter(key: Char): Unit = { }
+    
+    override def onMouseMove(x: Int, y: Int): Unit = {
+        //TODO: hover button
+    }
+    
+    override def onClick(x: Int, y: Int): Unit = {
+        val point = new Point(x, y)
+        val button = buttons find(view => view.rectangle contains(point)) get
+        
+        if (button != null) {
+            button onClick()
+        }
+    }
+}
