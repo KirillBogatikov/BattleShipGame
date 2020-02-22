6f79bbc0de4af80c82d8e27756b250e2f37adf95
Kirill Bogatikov <kivi15penza@gmail.com>
2020-02-22 23:58:55 +0300

    GameModeScreen abstract class

---
+++ render/src/main/scala/org/battleshipgame/ui/GameModeScreen.scala
@@ -0,0 +1,55 @@
+package org.battleshipgame.ui
+
+import scala.language.postfixOps
+import org.battleshipgame.render.Screen
+import org.battleshipgame.render.View
+import org.battleshipgame.render.Renderer
+import org.battleshipgame.render.Rectangle
+import org.battleshipgame.render.InputListener
+import org.battleshipgame.render.Point
+
+object GameModeScreen {
+    protected var instance: GameModeScreen = null
+    
+    def impl(): GameModeScreen = {
+        return instance;
+    }
+}
+
+abstract class GameModeScreen extends Screen with InputListener {
+    GameModeScreen.instance = this
+    
+    def singleGame(): View
+    def multiPlayer(): View
+    
+     override def render(renderer: Renderer): Unit = {
+        renderer begin()
+        
+        renderer image(new Rectangle(0, 0, size width, size height), content background)
+        
+        button(renderer, singleGame)
+        button(renderer, multiPlayer)
+        
+        renderer end()
+    }
+    
+    override def update(renderer: Renderer): Unit = {
+        //TODO: hover button
+    }
+    
+    override def onKeyEnter(key: Char): Unit = { }
+    
+    override def onMouseMove(x: Int, y: Int): Unit = {
+        //TODO: hover button
+    }
+    
+    override def onClick(x: Int, y: Int): Unit = {
+        val point = new Point(x, y)
+        
+        if (singleGame.rectangle contains(point)) {
+            singleGame onClick()
+        } else if (multiPlayer.rectangle contains(point)) {
+            multiPlayer onClick();   
+        }
+    }
+}
