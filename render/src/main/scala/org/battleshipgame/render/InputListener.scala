02b7db90af074b5cae8da9de696ccab0b0d43531
Kirill Bogatikov <kivi15penza@gmail.com>
2020-02-23 03:34:05 +0300

    onMouseUp & onMouseDown in InputListener

--- render/src/main/scala/org/battleshipgame/render/InputListener.scala
+++ render/src/main/scala/org/battleshipgame/render/InputListener.scala
@@ -3,5 +3,7 @@ package org.battleshipgame.render
 trait InputListener {
     def onKeyPress(key: Int): Unit
     def onMouseMove(x: Int, y: Int): Unit
+    def onMouseUp(x: Int, y: Int): Unit
+    def onMouseDown(x: Int, y: Int): Unit
     def onClick(x: Int, y: Int): Unit
 }
