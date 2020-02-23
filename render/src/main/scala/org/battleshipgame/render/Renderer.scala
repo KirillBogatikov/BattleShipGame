edcccd27c279e5a4eebc5dfd888192275d54439a
Kirill Bogatikov <kivi15penza@gmail.com>
2020-02-23 03:34:54 +0300

    added stroke(Int) and line(Int, Int, Int, Int) methods

--- render/src/main/scala/org/battleshipgame/render/Renderer.scala
+++ render/src/main/scala/org/battleshipgame/render/Renderer.scala
@@ -11,16 +11,19 @@ trait Renderer {
 
     def stroke(color: Long): Unit
     def fill(color: Long): Unit
+    
+    def stroke(size: Int): Unit
 
     def polygon(vertices: Array[Point]): Unit
     def rectangle(rectangle: Rectangle): Unit
-    def rectangle(vertex: Point, size: Size): Rectangle = {
-        val rect = new Rectangle(vertex x, vertex y, size width, size height)
+    def rectangle(vertex: Point, size: Size, cornerRadius: Double = 0.0): Rectangle = {
+        val rect = new Rectangle(vertex x, vertex y, size width, size height, cornerRadius)
         rectangle(rect)
         return rect
     }
     def arc(start: Point, corner: Point, end: Point): Unit
     def line(start: Point, end: Point): Unit
+    def line(sx: Int, sy: Int, ex: Int, ey: Int): Unit
 
     def image(rectangle: Rectangle, image: Image): Unit
 
