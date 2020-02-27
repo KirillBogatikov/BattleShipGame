e553805301c3f57bf57db6796b057d37ef6ca09b
Kirill Bogatikov <kivi15penza@gmail.com>
2020-02-28 00:20:39 +0300

    implementations of StartScreen and GameModeScreen

---
+++ desktop/src/main/java/org/battleshipgame/ui/DesktopGameModeScreen.java
@@ -0,0 +1,66 @@
+package org.battleshipgame.ui;
+
+import org.battleshipgame.render.Button;
+import org.battleshipgame.render.ClickListener;
+import org.battleshipgame.render.Image;
+import org.battleshipgame.render.ImageView;
+import org.battleshipgame.render.Renderer;
+import org.battleshipgame.render.Size;
+import org.battleshipgame.render.StylesResolver;
+import org.battleshipgame.render.TextView;
+
+public class DesktopGameModeScreen extends GameModeScreen {
+	private StylesResolver styles;
+	private Renderer renderer;
+	private Button singleplay, multiplayer;
+	private ImageView backView;
+	private TextView backLabel;
+	
+	public DesktopGameModeScreen(Image backImage, StylesResolver styles, Renderer renderer) {
+		this.styles = styles;
+		this.renderer = renderer;
+		
+		singleplay = new Button(224, 170, 200, 200, 20, "ОДИНОЧНАЯ\nИГРА", 24.0);
+		multiplayer = new Button(526, 170, 200, 200, 20, "СЕТЕВАЯ\nИГРА", 24.0);
+		backView = new ImageView(25, 50, 50, 50, backImage);
+		backLabel = new TextView(75, 50, 100, 50, "НАЗАД", 24.0, true);
+	}
+	
+	public void setClickListeners(ClickListener singleplay, ClickListener multiplayer, ClickListener back) {
+		this.singleplay.listener_$eq(singleplay);
+		this.multiplayer.listener_$eq(multiplayer);
+		this.backView.listener_$eq(back);
+		this.backLabel.listener_$eq(back);
+	}
+
+	@Override
+	public Size size() {
+		return new Size(960, 540);
+	}
+
+	@Override
+	public Renderer renderer() {
+		return renderer;
+	}
+
+	@Override
+	public StylesResolver styles() {
+		return styles;
+	}
+
+	@Override
+	public Button[] buttons() {
+		return new Button[]{ singleplay, multiplayer };
+	}
+
+	@Override
+	public ImageView[] images() {
+		return new ImageView[]{ backView };
+	}
+	
+	@Override
+	public TextView[] labels() {
+		return new TextView[] { backLabel };
+	}
+
+}
---
+++ desktop/src/main/java/org/battleshipgame/ui/DesktopStartScreen.java
@@ -0,0 +1,57 @@
+package org.battleshipgame.ui;
+
+import org.battleshipgame.render.Button;
+import org.battleshipgame.render.ClickListener;
+import org.battleshipgame.render.Renderer;
+import org.battleshipgame.render.Size;
+import org.battleshipgame.render.StylesResolver;
+import org.battleshipgame.render.TextView;
+
+public class DesktopStartScreen extends StartScreen {
+	private StylesResolver styles;
+	private Renderer renderer;
+	private Button startGame, continueGame, settings, exit;
+	
+	public DesktopStartScreen(StylesResolver styles, Renderer renderer) {
+		this.styles = styles;
+		this.renderer = renderer;
+		
+		startGame = new Button(50, 120, 250, 60, 20, "НОВАЯ ИГРА", 24.0);
+		continueGame = new Button(50, 200, 250, 60, 20, "ПРОДОЛЖИТЬ ИГРУ", 24.0);
+		settings = new Button(50, 280, 250, 60, 20, "НАСТРОЙКИ", 24.0);
+		exit = new Button(50, 360, 250, 60, 20, "ВЫХОД", 24.0);
+	}
+	
+	public void setClickListeners(ClickListener startGame, ClickListener continueGame, ClickListener settings) {
+		this.startGame.listener_$eq(startGame);
+		this.continueGame.listener_$eq(continueGame);
+		this.settings.listener_$eq(settings);
+		this.exit.listener_$eq(() -> System.exit(0));
+	}
+
+	@Override
+	public Size size() {
+		return new Size(960, 540);
+	}
+
+	@Override
+	public Renderer renderer() {
+		return renderer;
+	}
+
+	@Override
+	public StylesResolver styles() {
+		return styles;
+	}
+
+	@Override
+	public Button[] buttons() {
+		return new Button[]{ startGame, continueGame, settings, exit };
+	}
+	
+	@Override
+	public TextView[] labels() {
+		return new TextView[] { new TextView(860, 440, 100, 100, "v. 2.0", 24.0) };
+	}
+
+}
