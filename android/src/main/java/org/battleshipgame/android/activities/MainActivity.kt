82a7ba98a5a89154f957093b0cf3392f982548e5
Kirill Ispolnov <ispolnov@smartway.today>
2020-04-11 14:40:48 +0300

    Kotlinization

---
+++ android/src/main/java/org/battleshipgame/android/activities/MainActivity.kt
@@ -0,0 +1,25 @@
+package org.battleshipgame.android.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import org.battleshipgame.android.GameInstance
+import org.battleshipgame.android.R
+
+class MainActivity : AppCompatActivity() {
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        GameInstance.create()
+    }
+
+    fun startNewGame(view: View) {
+        val intent = Intent(this, GameModeActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun exit(view: View?) {
+        finish()
+    }
+}
